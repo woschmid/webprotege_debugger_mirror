383b27336b37fad8da671b63afc8ed154722de43
Wolfgang Schmid <Wolfgang.Schmid@aau.at>
2020-08-17 23:22:29 +0200

    Added proof of concept implementation of diagnosis and query generation for start debugging button

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/debugger/StartDebuggingActionHandler.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/debugger/StartDebuggingActionHandler.java
@@ -3,25 +3,57 @@ package edu.stanford.bmir.protege.web.server.debugger;
 import edu.stanford.bmir.protege.web.server.access.AccessManager;
 import edu.stanford.bmir.protege.web.server.dispatch.AbstractProjectActionHandler;
 import edu.stanford.bmir.protege.web.server.dispatch.ExecutionContext;
+import edu.stanford.bmir.protege.web.server.revision.RevisionManager;
 import edu.stanford.bmir.protege.web.shared.debugger.StartDebuggingAction;
 import edu.stanford.bmir.protege.web.shared.debugger.StartDebuggingResult;
+import edu.stanford.bmir.protege.web.shared.project.ProjectId;
+import org.exquisite.core.DiagnosisException;
 import org.exquisite.core.conflictsearch.QuickXPlain;
+import org.exquisite.core.engines.AbstractDiagnosisEngine;
 import org.exquisite.core.engines.HSTreeEngine;
 import org.exquisite.core.engines.IDiagnosisEngine;
+import org.exquisite.core.model.Diagnosis;
 import org.exquisite.core.model.DiagnosisModel;
+import org.exquisite.core.query.Query;
+import org.exquisite.core.query.querycomputation.heuristic.HeuristicConfiguration;
+import org.exquisite.core.query.querycomputation.heuristic.HeuristicQueryComputation;
+import org.exquisite.core.solver.ConsistencyChecker;
 import org.exquisite.core.solver.ExquisiteOWLReasoner;
 import org.exquisite.core.solver.ISolver;
+import org.semanticweb.HermiT.ReasonerFactory;
+import org.semanticweb.owlapi.model.OWLLogicalAxiom;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semarglproject.vocab.OWL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class StartDebuggingActionHandler extends AbstractProjectActionHandler<StartDebuggingAction, StartDebuggingResult> {
 
+    private static final Logger logger = LoggerFactory.getLogger(StartDebuggingActionHandler.class);
+
+    @Nonnull
+    private final RevisionManager revisionManager;
+
+    @Nonnull
+    private final ProjectId projectId;
+
     @Inject
-    public StartDebuggingActionHandler(@Nonnull AccessManager accessManager) {
+    public StartDebuggingActionHandler(@Nonnull AccessManager accessManager,
+                                       @Nonnull RevisionManager revisionManager,
+                                       @Nonnull ProjectId projectId) {
         super(accessManager);
+        this.revisionManager = checkNotNull(revisionManager);
+        this.projectId = checkNotNull(projectId);
     }
 
     @Nonnull
@@ -34,46 +66,72 @@ public class StartDebuggingActionHandler extends AbstractProjectActionHandler<St
     @Override
     public StartDebuggingResult execute(@Nonnull StartDebuggingAction action, @Nonnull ExecutionContext executionContext) {
         try {
-            // HermiT reasoner availability test
-            org.semanticweb.HermiT.ReasonerFactory reasonerFactory = new org.semanticweb.HermiT.ReasonerFactory();
 
-            // diagnosis module availabilty test
-            ISolver<Integer> solver = new ISolver<Integer>() {
-                @Override
-                public boolean isConsistent(Collection<Integer> collection) {
-                    return false;
+            final OWLOntologyManager owlOntologyManager = this.revisionManager.getOntologyManagerForRevision(this.revisionManager.getCurrentRevision());
+            final List<OWLOntology> ontologies = new ArrayList<>(owlOntologyManager.getOntologies());
+            if (ontologies.size() == 1) {
+                final OWLOntology ontology = ontologies.get(0);
+                logger.info("{} has ontology {})", projectId, ontology);
+
+                // create a solver using the ontology
+                final ExquisiteOWLReasoner solver = createSolver(ontology);
+                logger.info("{} solver created {}", projectId, solver);
+
+                // create diagnosis engine using the solver
+                IDiagnosisEngine diagnosisEngine = getDiagnosisEngine(solver);
+                logger.info("{} diagnosis engine created: {}", projectId, diagnosisEngine);
+
+                // prepare for diagnosis calculation
+                diagnosisEngine.resetEngine();
+                diagnosisEngine.setMaxNumberOfDiagnoses(0); // 0 means calculate all possible diagnoses
+
+                // calculate diagnoses
+                final Set diagnoses = diagnosisEngine.calculateDiagnoses();
+                logger.info("{} got {} diagnoses: {}", projectId, diagnoses.size(), diagnoses);
+
+                // calculate queries
+                if (diagnoses.size() >= 2) {
+                    HeuristicConfiguration<OWLLogicalAxiom> heuristicConfiguration = new HeuristicConfiguration<>((AbstractDiagnosisEngine)diagnosisEngine, null);
+                    final HeuristicQueryComputation<Object> queryComputation = new HeuristicQueryComputation<>(heuristicConfiguration);
+
+                    queryComputation.initialize(diagnoses);
+
+                    if (queryComputation.hasNext()) {
+                        final Query<Object> query = queryComputation.next();
+                        logger.info("{} computed query {}", projectId, query);
+                        return new StartDebuggingResult(query.toString());
+                    } else {
+                        logger.info("{} no query computed", projectId);
+                        return new StartDebuggingResult("No query computed");
+                    }
+                } else {
+                    return new StartDebuggingResult("Diagnosis found!");
                 }
 
-                @Override
-                public boolean isEntailed(Collection<Integer> collection, Collection<Integer> collection1) {
-                    return false;
-                }
-
-                @Override
-                public Set<Integer> calculateEntailments(Collection<Integer> collection) {
-                    return null;
-                }
-
-                @Override
-                public DiagnosisModel<Integer> getDiagnosisModel() {
-                    return null;
-                }
-
-                @Override
-                public void dispose() {
-
-                }
-            };
-            IDiagnosisEngine diagnosisEngine = getDiagnosisEngine(solver);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            return new StartDebuggingResult("Hi, you pressed the Start button!");
+            } else {
+                logger.error("Only one ontology expected but there were {} ongologies", ontologies.size());
+                return new StartDebuggingResult("Hmm, expected only one ontology!");
+            }
+        } catch (RuntimeException | DiagnosisException | OWLOntologyCreationException e) {
+            logger.error(e.getMessage(), e);
+            return new StartDebuggingResult(e.getMessage());
         }
     }
 
-    protected IDiagnosisEngine getDiagnosisEngine(ISolver solver) {
+    private IDiagnosisEngine getDiagnosisEngine(ISolver solver) {
         return new HSTreeEngine<>(solver, new QuickXPlain(solver));
+    }
+
+    private ExquisiteOWLReasoner createSolver(OWLOntology ontology) throws OWLOntologyCreationException {
+
+        // HermiT reasoner factory
+        final ReasonerFactory reasonerFactory = new ReasonerFactory();
+
+        // create a diagnosis model from the ontology
+        DiagnosisModel<OWLLogicalAxiom> diagnosisModel = ExquisiteOWLReasoner.generateDiagnosisModel(ontology, null);
+        diagnosisModel = ConsistencyChecker.checkConsistency(ontology, diagnosisModel, reasonerFactory, true, true, null, null).getDiagnosisModel();
+
+        // creates a reasoner using HermiT and the diagnosis model from the ontology provided
+        return new ExquisiteOWLReasoner(diagnosisModel, reasonerFactory);
     }
 }
