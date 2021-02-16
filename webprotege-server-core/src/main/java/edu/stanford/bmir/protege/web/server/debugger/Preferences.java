0b02db627b5f7fa683c1a97f251927fbd1ab6774
Wolfgang Schmid <Wolfgang.Schmid@aau.at>
2021-02-16 19:46:01 +0100

    Added setter methods with boundary checks for MAX_VISIBLE_CORRECT_AXIOMS, MAX_VISIBLE_POSSIBLY_FAULTY_AXIOMS and SESSION_KEEPALIVE_IN_MILLIS.

--- webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/debugger/Preferences.java
+++ webprotege-server-core/src/main/java/edu/stanford/bmir/protege/web/server/debugger/Preferences.java
@@ -23,13 +23,32 @@ public class Preferences {
      */
     static int MAX_VISIBLE_CORRECT_AXIOMS = 10;
 
-
+    /**
+     * Possibility to change SESSION_KEEPALIVE_IN_MILLIS to a value in milliseconds between 1 minute to 6 hours.
+     * @param sessionKeepaliveInMillis Time in milliseconds [1 minute ... 6 hours]
+     */
     protected static void setSessionKeepaliveInMillis(Long sessionKeepaliveInMillis) {
-        SESSION_KEEPALIVE_IN_MILLIS = sessionKeepaliveInMillis;
-        REASONER_TIMEOUT_IN_MILLIS = SESSION_KEEPALIVE_IN_MILLIS * 90 / 100;
+        if (sessionKeepaliveInMillis >= 1000L * 60L && sessionKeepaliveInMillis <= 1000L * 60L * 60L + 6L) {
+            SESSION_KEEPALIVE_IN_MILLIS = sessionKeepaliveInMillis;
+            REASONER_TIMEOUT_IN_MILLIS = SESSION_KEEPALIVE_IN_MILLIS * 90 / 100;
+        }
     }
 
+    /**
+     * Possibility to change MAX_VISIBLE_POSSIBLY_FAULTY_AXIOMS to a value between 1 and 50.
+     * @param maxVisiblePossiblyFaultyAxioms Value between [1 .. 50]
+     */
     protected static void setMaxVisiblePossiblyFaultyAxioms(int maxVisiblePossiblyFaultyAxioms) {
-        MAX_VISIBLE_POSSIBLY_FAULTY_AXIOMS = maxVisiblePossiblyFaultyAxioms;
+        if (maxVisiblePossiblyFaultyAxioms > 0 && maxVisiblePossiblyFaultyAxioms <= 50)
+            MAX_VISIBLE_POSSIBLY_FAULTY_AXIOMS = maxVisiblePossiblyFaultyAxioms;
+    }
+
+    /**
+     * Possibility to change MAX_VISIBLE_CORRECT_AXIOMS to a value between 1 and 50.
+     * @param maxVisibleCorrectAxioms Value between [1 .. 50]
+     */
+    protected static void setMaxVisibleCorrectAxioms(int maxVisibleCorrectAxioms) {
+        if (maxVisibleCorrectAxioms > 0 && maxVisibleCorrectAxioms <= 50)
+            MAX_VISIBLE_CORRECT_AXIOMS = maxVisibleCorrectAxioms;
     }
 }
